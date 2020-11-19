10b8ff61468f7c3a9d5ce7d9e587f622be6c3f21
pcanalesm <63012797+pcanalesta@users.noreply.github.com>
2020-11-18 22:11:38 -0300

    18-11

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    MONGO_URI: process.env.MONGO_URI || 'mongodb+srv://pcanales:alaska1944@cluster0.gma7o.mongodb.net/pcm-bank-chll?retryWrites=true&w=majority',
-    SALT_FACTOR: process.env.SALT_FACTOR || 10,
-    PORT: process.env.PORT || 5000,
-    CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:4200',
-    JWT_SECRET: process.env.JWT_SECRET || 'bed764f2-0a54-4844-a91d-2394ece42a4e',
-    SESSION_EXPIRE: process.env.SESSION_EXPIRE || '1h'
+    MONGO_URI: process.env.MONGO_URI,
+    SALT_FACTOR: process.env.SALT_FACTOR,
+    PORT: process.env.PORT,
+    CORS_ORIGIN: process.env.CORS_ORIGIN,
+    JWT_SECRET: process.env.JWT_SECRET,
+    SESSION_EXPIRE: process.env.SESSION_EXPIRE
 }
